796c3a25db1a76122f8ac8bfb8f78947803e9815
mohangowda8055 <mohangowda8055k@gmail.com>
2024-10-15 22:16:40 +0530

    updated axios base-url

--- qllb/src/axios/custom.js
+++ qllb/src/axios/custom.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "https://ec2-43-204-116-202.ap-south-1.compute.amazonaws.com:8080/api/v1",
+  baseURL: "http://ec2-43-204-116-202.ap-south-1.compute.amazonaws.com:8080/api/v1",
 });
 
 customFetch.interceptors.request.use(
